1f1a666c68ddb000d8e2de94b1f6a4931cc9f953
Tore Aurstad <tore.aurstad@hemit.no>
2020-10-07 23:43:45 +0200

    Nuget metadata oppsett for Eslint standalone verktøy

---
+++ nuget_upload/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "plugins": ["ie11"],
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": false
+  },
+  "parserOptions": {
+    "ecmaVersion": 5,
+  },
+  "rules": {
+    "ie11/no-collection-args": ["error"],
+    "ie11/no-for-in-const": ["error"],
+    //"ie11/no-loop-func": ["warn"],
+    "ie11/no-weak-collections": ["error"],
+    "curly": ["off"]
+  }
+};
+
+//A list of rules that can be applied is here: https://eslint.org/docs/rules/
+//The rules can have the following severity in EsLint: "warn", "error" and "off".
