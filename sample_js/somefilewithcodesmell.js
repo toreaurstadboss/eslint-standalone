2dbfe5ac58790dba2e944dc0b4cccb575f43aa30
Tore Aurstad <tore.aurstad@hemit.no>
2020-10-07 17:50:27 +0200

    Adding VsCode launch configuration and a sample folder

---
+++ sample_js/somefilewithcodesmell.js
@@ -0,0 +1,27 @@
+var materials = [
+  'Hydrogen',
+  'Helium',
+  'Lithium',
+  'Beryllium'
+];
+
+console.log(materials.map(material => material.length));
+
+
+
+var countString = '4';
+var count1 = parseInt(countString);
+if (count1 == 4)
+  console.log('countString is four');
+
+let somefunvar = 123;
+
+const MathPI = 3.141592;
+
+
+
+
+
+
+// expected output: Array [8, 6, 7, 9]
+
